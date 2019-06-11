b5e643add90ddc86097fa21e3244b0d820ec8dfb
seryozhko <seryozhko@outlook.com>
2019-06-11 20:48:51 +0300

    single post template added

--- functions.php
+++ functions.php
@@ -20,12 +20,24 @@
 
   add_filter('excerpt_length', 'bs_set_excerpt_length');
 
-  function bs_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
-    if ( 'post-thumbnail' === $size ) {
-      is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
-      ! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
-    }
-    return $attr;
-  }
-  add_filter( 'wp_get_attachment_image_attributes', 'bs_post_thumbnail_sizes_attr', 10 , 3 );
+  // function bs_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+  //   if ( 'post-thumbnail' === $size ) {
+  //     is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
+  //     ! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
+  //   }
+  //   return $attr;
+  // }
+  // add_filter( 'wp_get_attachment_image_attributes', 'bs_post_thumbnail_sizes_attr', 10 , 3 );
   
+  // function bs_content_image_sizes_attr( $sizes, $size ) {
+  //   $width = $size[0];
+  //   840 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 62vw, 840px';
+  //   if ( 'page' === get_post_type() ) {
+  //     840 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
+  //   } else {
+  //     840 > $width && 600 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 61vw, (max-width: 1362px) 45vw, 600px';
+  //     600 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
+  //   }
+  //   return $sizes;
+  // }
+  // add_filter( 'wp_calculate_image_sizes', 'bs_content_image_sizes_attr', 10 , 2 );
